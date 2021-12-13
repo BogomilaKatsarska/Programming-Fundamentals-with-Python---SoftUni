138c026588942bf8d97337138b751eca2b7f6d1a
BogomilaKatsarska <90954520+BogomilaKatsarska@users.noreply.github.com>
2021-12-13 16:14:10 +0200

    Add files via upload

---
+++ Basic Syntaxis, Conditional Statements and Loops - E1Q2.py
@@ -0,0 +1,19 @@
+# 2.	Drink Something
+# Kids drink toddy, teens drink coke, young adults drink beer, adults drink whisky. Create a program which receives an age and prints what they drink.
+# Rules:
+# Kid is defined as someone under the age of 14.
+# Teen is defined as someone under the age of 18.
+# Young adult is defined as someone under the age of 21.
+# Adult is defined as someone above the age of 21.
+# Note: All the values are inclusive except the last one!
+
+age = int(input())
+
+if age <= 14:
+    print("drink toddy")
+elif age <= 18:
+    print("drink coke")
+elif age <= 21:
+    print("drink beer")
+else:
+    print("drink whisky")
